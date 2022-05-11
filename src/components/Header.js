19509af3cc77fd259ec22cf9b311718c8384f7e8
Mclilzee <mc.lilzee@hotmail.com>
2022-05-11 23:02:24 +0200

    Add new fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ export default function Header(props) {
     return (
         <div className={"header"}>
             <h1>Memory card game</h1>
-            <h4>To score, you have to choose each card only once.</h4>
+            <h4>To score, you have to choose each card only once. If you selected all the cards, you will be able to
+                repeat the process, increasing the score even further!</h4>
         </div>
     )
 }
